a5b381774c4f5fc0044156b90fdb99f351977109
Ian Wagner <ianwagnerlautaro@gmail.com>
2020-09-22 20:25:43 -0300

    correccion persona

--- src/PruebaPack/Persona.java
+++ src/PruebaPack/Persona.java
@@ -3,8 +3,14 @@ package PruebaPack;
 public class Persona {
 	
 	private String nombre;
+	private String apellido;
 	
 	public String getNombre() {
+		
+		return nombre;
+	}
+	
+	public String getApellidp() {
 		
 		return nombre;
 	}
